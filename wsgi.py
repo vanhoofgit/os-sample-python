9d9d64453b838078a6a0617291a7e0656c193d27
Van Hoof Benny <benny.vanhoof@be.ibm.com>
2021-05-20 16:15:04 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello Dear Wankers!"
+    return "Hello Dear niquetameres!"
 
 if __name__ == "__main__":
     application.run()
